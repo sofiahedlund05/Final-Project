5ff672aec5399605cca693ac233d2145464239e4
sofiahedlund05 <sofiahedlund@gmail.com>
2023-03-17 09:06:19 -0500

    Initial Commit

---
+++ APapp/APappApp.swift
@@ -0,0 +1,17 @@
+//
+//  APappApp.swift
+//  APapp
+//
+//  Created by Sofia Hedlund23 on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct APappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
